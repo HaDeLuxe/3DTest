2144be1d8b035115a256535a237cb326f2306c21
Pascal Haas <haas_pa@hotmail.de>
2018-09-04 17:56:20 +0200

    Animation

--- 3D Übung/3D Übung/Game1.cs
+++ 3D Übung/3D Übung/Game1.cs
@@ -10,9 +10,21 @@ namespace _3D_Übung {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+
+        private Model model;
+
+        private Matrix world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+        private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);
+        private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 100f);
+
+        Vector3 position;
+        private float angleY;
+
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            position = new Vector3(0, 0, 0);
+            angleY = 0;
             //Github Test
         }
 
@@ -36,6 +48,8 @@ namespace _3D_Übung {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
+            model = Content.Load<Model>("3D Model\\Cube");
             // TODO: use this.Content to load your game content here
         }
 
@@ -56,7 +70,15 @@ namespace _3D_Übung {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
             // TODO: Add your update logic here
+
+            if(Keyboard.GetState().IsKeyUp(Keys.W)) position += new Vector3(0, 0.01f, 0);
+            if (Keyboard.GetState().IsKeyUp(Keys.S)) position -= new Vector3(0, 0.01f, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.A)) angleY -= .1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.D)) angleY += .1f;
+
+            world = Matrix.CreateRotationY(angleY) * Matrix.CreateTranslation(position);
 
             base.Update(gameTime);
         }
@@ -69,8 +91,26 @@ namespace _3D_Übung {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            DrawModel(model, world, view, projection);
             base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection) 
+        {
+            foreach(ModelMesh mesh in model.Meshes)
+            {
+                foreach(BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+
+
+
         }
     }
 }
---
+++ 3D Übung/3D Übung/Game1Extended.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Übung {
+    class Game1Extended {
+
+        //Translation Matrix
+
+        //Matrix.CreateTranslation(Vector3 position)
+
+        //Rotation around X Axis
+
+        //Matrix.CreateRotationX(float angleInRadians);
+
+        //Rotation around Y Axis
+
+        //Matrix.CreateRotationY(float angleInRadians);
+
+        //Rotation around Z Axis
+
+        //Matrix.CreateRotationZ(float angleInRadians);
+
+        //Rotation aroung arbitrary axis
+
+        //Matrix.CreateFromAxisAngle(Vector3 axis, float angleInRadians);
+
+        //Combine Matrixed
+
+        Matrix result = Matrix.CreateRotationX(MathHelper.ToRadians(45)) * Matrix.CreateTranslation(new Vector3(10, 0, 0));
+
+
+
+        //Creating a view Matrix
+     
+        //Matrix.CreateLookAt(Vector3 cameraPosition, Vector3 cameraTarget, Vector3 cameraUpVector);
+        /*
+         cameraPosition: Location of the camera in 3d World
+         cameraTarget: point in the world that the camera is looking at
+         cameraUpVector: Direction that is up for the camera.
+         */
+
+        //Creating an orthographic projection
+        //Matrix.CreateOrthographic(float widht, float height, float zNearPlane, float zFarPlane);
+
+
+        //First method to create a persective projection
+        //Matrix.CreatePerspectiveFieldOfView(float fieldOfView, float aspectRatio, float nearPlaneDistance, float farPlaneDistance);
+        //aspectRatio = graphics.PreferredBackPufferWidht / graphics.PreferredBackPufferHeight;
+
+        //Second method to create a perspectice projection
+        //Matrix.CreatePerspective(float width, float height, flaot nearPlaneDistance, float farPlaneDistance);
+        /*
+        width and height are the sizes of the viewing area ath the near clipping plane.
+         */
+
+        /*
+         Custom Matrix:
+
+        Matrix customMatrix = new Matrix(
+        float m11, float m12, float m13,
+        float m21, float m22, float m23,
+        float m31, float m32, float m33);
+         
+         */
+    }
+}
